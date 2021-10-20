916d42bc960326db5cccb9f5166ed5ceb8918d30
Robson Santos Santana <robsonsanttos_21@hotmail.com>
2021-10-20 15:03:13 -0300

    Introdução
    Iniciando com o react-js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+    return ( <
+        div className = "App" >
+
+        <
+        div > contador: { counter } < /div>  <
+        button onClick = { increment } > Incremento < /button> < /
+        div >
+    );
 }
 
 export default App;
