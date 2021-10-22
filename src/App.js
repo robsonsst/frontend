fb1485b2e77402fc4bb54926add1209050bde97a
Robson Santos Santana <robsonsanttos_21@hotmail.com>
2021-10-22 15:41:50 -0300

    Integração de api com axios
    Criando a api e trazendo dados do backend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { useState } from "react";
+import User from './pages/User'
 
 function App() {
 
-    const [counter, setCounter] = useState(0);
-
-    function increment() {
-        setCounter(counter + 1);
-    }
-    return ( <
-        div className = "App" >
+    return ( < div className = "App" >
 
         <
-        div > contador: { counter } < /div>  <
-        button onClick = { increment } > Incremento < /button> < /
-        div >
+        User / > <
+        /div >
     );
 }
 
