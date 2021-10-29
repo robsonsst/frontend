fdb3ecf0a9ac1065832f9f7db7aa3c3df80a1df8
Robson Santos Santana <robsonsanttos_21@hotmail.com>
2021-10-29 16:46:20 -0300

    rotas, navegação e CRUD
    rotas, navegação e CRUD parte 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import User from './pages/User'
+import Routes from "./routes";
 
 function App() {
 
     return ( < div className = "App" >
 
         <
-        User / > <
+        Routes / > <
         /div >
     );
 }
---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import User from './pages/User';
+import Profile from './pages/Profile'
+
+
+export default function Routes() {
+    return ( <
+        BrowserRouter >
+        <
+        Switch >
+        <
+        Route exact path = "/"
+        component = {
+            User
+        }
+        / >  <
+        Route path = "/create"
+        component = {
+            Profile
+        }
+        /> <
+        Route path = "/update/:id"
+        component = {
+            Profile
+        }
+        / > </
+        Switch > < /
+        BrowserRouter > );
+
+}
